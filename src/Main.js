bb1e5eac1fa27c48f2c4f7658d7eac058c896a0e
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-08-26 14:44:40 -0400

    basic sidebar and logo styling done

--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,7 @@ import NoteForm from './NoteForm'
 class Main extends React.Component{
     render(){
         return(
-            <div className="Main">
+            <div className="Main" style = {style}>
             <Sidebar />
             <NoteList />
             <NoteForm />
@@ -16,3 +16,9 @@ class Main extends React.Component{
 }
 
 export default Main
+
+const style = {
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'stretch',
+  }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,58 @@
 import React from 'react'
+import quill from './quill.svg'
+import newIcon from './new.png'
+import newHover from './new-hover.png'
 
 class Sidebar extends React.Component{
     render(){
         return(
-            <div className = "Sidebar">
-              Sidebar
+            <div className = "Sidebar"  style={styles.sidebar}>
+
+             <div className="logo" style={styles.logo}>
+        <img src={quill} alt="Noteherder" style={styles.logoImg} />
+      </div>
+
+      <a href="/notes">
+        <img
+          src={newHover}
+          alt="New note"
+        />
+        <img
+          src={newIcon}
+          alt="New note"
+        />
+      </a>
+
+<div className="SignOut">
+        <button>
+          <i className="fa fa-sign-out"></i>
+        </button>
+      </div>
             </div>
         )
     }
 }
 
 export default Sidebar
+
+const styles = {
+    sidebar: {
+        width: '6rem',
+        backgroundColor: '#f3f3f3',
+        padding: '0.5rem 0',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+
+    logo: {
+        fontFamily: '"Fauna One"',
+        color: '#666',
+        fontSize: '3rem',
+      },
+
+        logoImg: {
+        width: '3rem',
+        paddingLeft: '0.4rem',
+      }
+}
