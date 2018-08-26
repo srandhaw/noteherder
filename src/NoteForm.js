984e1a3602573a8763affb1de797d1c82721b9de
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-08-26 18:19:47 -0400

    note form and notelist layout made

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -3,8 +3,23 @@ import React from 'react'
  const NoteForm = () => {
   return (
     <div className="NoteForm">
-      NoteForm
+    
+    <div className="form-actions">
+      <button type="button">
+        <i className="far fa-trash-alt"></i>
+      </button>
     </div>
+    <form>
+      <p>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title your note"
+        />
+      </p>
+       <textarea name="body"></textarea>
+    </form>
+  </div>
   )
 }
 
