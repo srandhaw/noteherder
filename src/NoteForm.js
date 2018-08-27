85c32db93a4a3f9ac4b53e3dc63afb70493fb5ed
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-08-27 00:37:18 -0400

    styled notelist and noteform completely

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,26 +1,28 @@
 import React from 'react'
 
- const NoteForm = () => {
+import './NoteForm.css'
+
+const NoteForm = () => {
   return (
     <div className="NoteForm">
-    
-    <div className="form-actions">
-      <button type="button">
-        <i className="far fa-trash-alt"></i>
-      </button>
+      <div className="form-actions">
+        <button type="button">
+          <i className="far fa-trash-alt"></i>
+        </button>
+      </div>
+      <form>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title your note"
+          />
+        </p>
+
+        <textarea name="body"></textarea>
+      </form>
     </div>
-    <form>
-      <p>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title your note"
-        />
-      </p>
-       <textarea name="body"></textarea>
-    </form>
-  </div>
   )
 }
 
- export default NoteForm
+export default NoteForm
--- src/NoteList.js
+++ src/NoteList.js
@@ -1,31 +1,29 @@
 import React from 'react'
 
- const NoteList = () => {
+import './NoteList.css'
+
+const NoteList = () => {
   return (
     <div className="NoteList">
-      <h3>Notes</h3>
-      <ul id = "notes">
-        <a className = "active">
-        <li>
-          <div className = "note">
-
-            <div className = "note-title">
-                      Title
-            </div>
-
-            <div className="note-body">
-                      <p>
-                        Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
-                      </p>
-            </div>
-
-
-            </div>
-        </li>
-        </a>
-      </ul>
-    </div>
+          <h3>Notes</h3>
+          <ul id="notes">
+            <a className="active">
+              <li>
+                <div className="note">
+                  <div className="note-title">
+                    Kohlrabi welsh
+                  </div>
+                  <div className="note-body">
+                    <p>
+                      Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </a>
+          </ul>
+        </div>
   )
 }
 
- export default NoteList
+export default NoteList
