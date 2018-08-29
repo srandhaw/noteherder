be57547443e6e1ef86f09ba3962eda9744558e57
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-08-29 19:10:14 -0400

    put notes in main and did set current note functionality

--- src/Main.js
+++ src/Main.js
@@ -4,11 +4,41 @@ import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
 class Main extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+
+            currentNote: null,
+
+          notes: [
+            {
+              id: 1,
+              title: 'Why I <3 JS',
+              body: 'Because I like code, and JS is code.',
+            },
+            {
+              id: 2,
+              title: 'Thoughts on breakfast',
+              body: 'I love it!',
+            },
+            {
+              id: 3,
+              title: 'Watching the first episode of Black Mirror with your parents',
+              body: 'Don\'t.',
+            },
+          ]
+        }
+      }
+
+      setCurrentNote = (note) => {
+        this.setState({ currentNote: note })
+      }
+
     render(){
         return(
             <div className="Main" style = {style}>
             <Sidebar />
-            <NoteList />
+            <NoteList notes={this.state.notes} setCurrentNote = {this.setCurrentNote}/>
             <NoteForm />
             </div>
         )
